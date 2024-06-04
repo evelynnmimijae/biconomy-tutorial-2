83eb08cf80a3762606e51b86d3a9de3112962425
Your Name <you@example.com>
2024-06-03 22:29:12 -0700

    Web3Auth Imports per Biconomy docs

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,1 +1,9 @@
 import { useEffect, useRef, useState } from "react";
+import {
+    createSmartAccountClient,
+    BiconomySmartAccountV2,
+    PaymasterMode,
+  } from "@biconomy/account";
+  import { ethers } from "ethers";
+  import { Web3Auth } from "@web3auth/modal";
+  import { CHAIN_NAMESPACES } from "@web3auth/base";
