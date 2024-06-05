7c2465914bb138cd32e9e0d8ee91a1df0ffd8e87
Your Name <you@example.com>
2024-06-04 20:42:31 -0700

    chainConfig

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -18,21 +18,26 @@ import {
     tickerName: "Polygon Matic",
   };
   
-  // Creating web3auth instance
-const web3auth = new Web3Auth({
-    clientId:
-      "BExrkk4gXp86e9VCrpxpjQYvmojRSKHstPRczQA10UQM94S5FtsZcxx4Cg5zk58F7W1cAGNVx1-NPJCTFIzqdbs", // Get your Client ID from the Web3Auth Dashboard https://dashboard.web3auth.io/
-    web3AuthNetwork: "sapphire_devnet", // Web3Auth Network
-    chainConfig,
-    // You can visit web3auth.io/docs for more configuration options
+  const web3auth = new Web3Auth({
+    clientId: "BBsGwU6xKi4LuZDmO_xYno4dnYpr5tj0aQFA1SPh3RUjcYabvOYQzNQZeIudtLGovPtnhjJxJ89uHUE-MBudCv4",
+    web3AuthNetwork: "sapphire_devnet",
+    chainConfig: {
+      chainNamespace: CHAIN_NAMESPACES.EIP155,
+      chainId: "0x13882",
+      rpcTarget: "https://rpc-amoy.polygon.technology/",
+      displayName: "Polygon Amoy",
+      blockExplorer: "https://www.oklink.com/amoy/",
+      ticker: "MATIC",
+      tickerName: "Polygon Matic",
+    },
     uiConfig: {
-      appName: "Biconomy X Web3Auth",
-      mode: "dark", // light, dark or auto
+      appName: "Your App Name",
+      mode: "dark", // light, dark, or auto
       loginMethodsOrder: ["apple", "google", "twitter"],
-      logoLight: "https://web3auth.io/images/web3auth-logo.svg",
-      logoDark: "https://web3auth.io/images/web3auth-logo---Dark.svg",
-      defaultLanguage: "en", // en, de, ja, ko, zh, es, fr, pt, nl
+      logoLight: "https://yourdomain.com/light-logo.svg",
+      logoDark: "https://yourdomain.com/dark-logo.svg",
+      defaultLanguage: "en", // Language codes supported by Web3Auth
       loginGridCol: 3,
-      primaryButton: "socialLogin", // "externalLogin" | "socialLogin" | "emailLogin"
+      primaryButton: "socialLogin", // Options: "externalLogin", "socialLogin", "emailLogin"
     },
   });
